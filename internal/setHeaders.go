213ad6027b055ff41eec22e85d7dea08daf5e400
Daniel-Sogbey <mathematics06physics@gmail.com>
2023-08-16 01:39:00 +0000

    pacth changes

--- internal/setHeaders.go
+++ internal/setHeaders.go
@@ -4,5 +4,6 @@ import "net/http"
 
 func SetHeaders(request *http.Request, authorization string) {
 	request.Header.Set("authorization", "Bearer "+authorization)
+
 	request.Header.Set("content-type", "application/json")
 }
